24edec5f05c4a82a58e3dac4428209bb75eae0d8
kontrol <kontrol_apa@protonmail.com>
2022-04-09 22:52:57 +0200

    added market and apa gal on chain stuff

--- src/APAGals.sol
+++ src/APAGals.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-
 
 
 contract PartyAnimals is
@@ -14,224 +12,217 @@ contract PartyAnimals is
     ReentrancyGuard,
     Ownable
 {
-    using Strings for uint256;
-    uint256 public constant MAX_MINTABLE = 1000;
-    uint256 public constant MAX_PER_CLAIM = 10;
-    uint256 public constant INITIAL_CLAIM_PRICE = 4 ether;
+
+    uint256 public constant MAX_SUPPLY = 1000;
+    uint256 public reserved = 900;
+    uint256 apaGalsMinted = 0;
+    uint256 public constant MAX_PER_CLAIM = 5;
+    uint256 public constant CLAIM_PRICE = 4 ether;
 
     bool public canClaim = false;
     bool public canClaimAirdrop = false;
     uint256 public numHonoraries = 0;
-    uint256 public minted = 0;
-
-    bytes32 merkleRoot;
-    uint256 public airdropNumber = 0;
-    mapping(address => uint256) redeemTracker;
-
-    string baseUri = "https://partyanimals.xyz/id/";
-
-
-
-    // tokenID mapping
     mapping(uint256 => uint256) indexer;
-    uint256 indexerLength = MAX_MINTABLE;
-    mapping(uint256 => uint256) tokenIDMap;
-    mapping(uint256 => uint256) takenImages;
 
-    event Claim(uint256 indexed _id);
-    event Redeem(uint256 indexed _id);
+    bytes32 freeMerkleRoot;
+    mapping(address => uint256) public freeRedeemed;
+
+    string baseURI = "https://partyanimals.xyz/apagals/id/";
+
+    event UnlabeledData(string key, uint256 tokenId);
 
 
-
-    /* *************** */
-    /*     Minting     */
-    /* *************** */
-    function getImageIDs(uint256 from, uint256 length) external view returns (uint256[] memory)  {
-        require(from + length <= minted, "Exceeds number of minted.");
-
-        uint256[] memory imageIDs = new uint256[](length);
-
-        for(uint256 i = 0; i < length; i++) {
-            imageIDs[i] = tokenIDMap[from + i]; 
-        }
-        
-        return imageIDs;
-    }
-
-    // Think of it as an array of 10_000 elements, where we take
-    //    a random index, and then we want to make sure we don't
-    //    pick it again.
-    // If it hasn't been picked, the mapping points to 0, otherwise
-    //    it will point to the index which took its place
-    function getNextImageID(uint256 index) internal returns (uint256) {
-        uint256 nextImageID = indexer[index];
-
-        // if it's 0, means it hasn't been picked yet
-        if (nextImageID == 0) {
-            nextImageID = index;
-        }
-        // Swap last one with the picked one.
-        // Last one can be a previously picked one as well, thats why we check
-        if (indexer[indexerLength - 1] == 0) {
-            indexer[index] = indexerLength - 1;
-        } else {
-            indexer[index] = indexer[indexerLength - 1];
-        }
-        indexerLength -= 1;
-        return nextImageID;
-    }
-
-    function enoughRandom() internal view returns (uint256) {
-        if (MAX_MINTABLE - minted == 0) return 0;
-        return
-            uint256(
-                keccak256(
-                    abi.encodePacked(
-                        block.difficulty,
-                        block.timestamp,
-                        msg.sender,
-                        blockhash(block.number)
-                    )
-                )
-            ) % (indexerLength);
-    }
-
-    function toggleClaimability() external onlyOwner {
-        canClaim = !canClaim;
-    }
-
-    function toggleAirdropClaimability() external onlyOwner {
-        canClaimAirdrop = !canClaimAirdrop;
-    }
-
-    function mintAPAGal(address receiver, uint256 nextTokenIndex) internal {
-        uint256 nextIndexerId = enoughRandom();
-        uint256 nextImageID = getNextImageID(nextIndexerId);
-        assert(takenImages[nextImageID] == 0);
-        takenImages[nextImageID] = 1;
-        tokenIDMap[nextTokenIndex] = nextImageID;
-        _safeMint(receiver, nextTokenIndex);
-    }
-
-    function claim(uint256 n) public payable nonReentrant {
-        require(
-            canClaim,
-            "Party Animals: it's not possible to claim just yet."
-        );
-        require(
-            MAX_MINTABLE - airdropNumber - minted > 0,
-            "Party Animals: not enough Party Animals left to mint."
-        );
-        require(n > 0, "Party Animals: not enough Party Animals left to mint.");
-        require(n <= MAX_PER_CLAIM);
-        if (n > MAX_MINTABLE - airdropNumber - minted) {
-            n = MAX_MINTABLE - airdropNumber - minted;
-        }
-
-        uint256 total_cost = n * 4 ether;
-        require(msg.value >= total_cost);
-
-        uint256 excess = msg.value - total_cost;
-        payable(address(this)).transfer(total_cost);
-
-        for (uint256 i = 0; i < n; i++) {
-            uint256 nextIndex = minted;
-            minted += 1;
-
-            mintAPAGal(_msgSender(), nextIndex);
-
-            emit Claim(nextIndex);
-        }
     
-        if(excess > 0) {
-            payable(_msgSender()).transfer(excess);
+
+    error MintLimit();
+    error InsufficientAmount();
+    error Unauthorized();
+    error ReservedAmountInvalid();
+
+    
+    /*///////////////////////////////////////////////////////////////
+                            FUTURE CONTENT RELATED
+    //////////////////////////////////////////////////////////////*/
+
+    mapping(string => mapping(uint256 => bytes32)) public unlabeledData;
+    mapping(address => bool) public contractAuthorities;
+
+    modifier onlyOwnerOrContractAuthority() {
+        if (msg.sender != owner() && !contractAuthorities[msg.sender]) revert Unauthorized();
+        _;
+    }
+
+    function setData(
+        string calldata _key,
+        uint256 _tokenId,
+        bytes32 _data
+    ) external onlyOwnerOrContractAuthority {
+        unlabeledData[_key][_tokenId] = _data;
+
+        emit UnlabeledData(_key, _tokenId);
+    }
+
+    function unsetData(string calldata _key, uint256 _tokenId)
+        external
+        onlyOwnerOrContractAuthority
+    {
+        delete unlabeledData[_key][_tokenId];
+    }
+
+    function getData(string calldata _key, uint256 _tokenId)
+        external
+        view
+        returns (bytes32)
+    {
+        return unlabeledData[_key][_tokenId];
+    }
+
+
+    function addcontractAuthorities(address[] calldata _contractAuthorities) external onlyOwner {
+        uint256 length = _contractAuthorities.length;
+        for (uint256 i; i < length; ) {
+            contractAuthorities[_contractAuthorities[i]] = true;
+            unchecked {
+                ++i;
+            }
         }
     }
 
-    /* ******************* */
-    /*      Airdrops       */
-    /* ******************* */
+    function removeContractAuthority(address _contractAuthority) external onlyOwner {
+        delete contractAuthorities[_contractAuthority];
+    }
+
+
+
+
+    /*///////////////////////////////////////////////////////////////
+                            APA GAL MINTING
+    //////////////////////////////////////////////////////////////*/
+
+    function _handleMint(uint256 numberOfMints) internal {
+        // solhint-disable-next-line
+        unchecked {
+            uint256 totalAPAGals = apaGalsMinted + numberOfMints;
+            if (
+                numberOfMints > MAX_PER_CLAIM ||
+                totalAPAGals > (MAX_SUPPLY - reserved)
+            ) revert MintLimit();
+
+            _mintApaGals(numberOfMints, totalAPAGals - numberOfMints);
+            apaGalsMinted = totalAPAGals;
+        }
+    }
+
+    function _mintApaGals(uint256 numberOfMints, uint256 totalApagalsBefore)
+        internal
+    {
+        uint256 seed = enoughRandom();
+
+        uint256 _indexerLength;
+        unchecked {
+            _indexerLength = MAX_SUPPLY - totalApagalsBefore;
+        }
+
+        for (uint256 i; i < numberOfMints; ) {
+            seed >>= i;
+
+            // Find the next available tokenID
+            //slither-disable-next-line weak-prng
+            uint256 index = seed % _indexerLength;
+            uint256 tokenId = indexer[index];
+
+            if (tokenId == 0) {
+                tokenId = index;
+            }
+
+            // Swap the picked tokenId for the last element
+            unchecked {
+                --_indexerLength;
+            }
+
+            uint256 last = indexer[_indexerLength];
+            if (last == 0) {
+                // this _indexerLength value had not been picked before
+                indexer[index] = _indexerLength;
+            } else {
+                // this _indexerLength value had been picked and swapped before
+                indexer[index] = last;
+            }
+
+            // Mint Hopper and generate its attributes
+            _mint(msg.sender, tokenId);
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function normalMint(uint256 numberOfMints) external payable {
+        require(canClaim);
+        if (CLAIM_PRICE * numberOfMints > msg.value) revert InsufficientAmount();
+
+        _handleMint(numberOfMints);
+    }
+
+    function freeMint(
+        uint256 numberOfMints,
+        uint256 totalGiven,
+        bytes32[] memory proof
+    ) external {
+        require(canClaimAirdrop);
+        if (freeRedeemed[msg.sender] + numberOfMints > totalGiven)
+            revert Unauthorized();
+        if (reserved < numberOfMints) revert ReservedAmountInvalid();
+        if (
+            !MerkleProof.verify(
+                proof,
+                freeMerkleRoot,
+                keccak256(abi.encodePacked(msg.sender, totalGiven))
+            )
+        ) revert Unauthorized();
+
+        unchecked {
+            freeRedeemed[msg.sender] += numberOfMints;
+            reserved -= numberOfMints;
+        }
+
+        _handleMint(numberOfMints);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                            AIRDROPS
+    //////////////////////////////////////////////////////////////*/
     function addHonoraries(address[] calldata honoraries) external onlyOwner {
-        uint256 honoraryID = numHonoraries + 1000;
+        uint256 honoraryID = numHonoraries + MAX_SUPPLY;
         for (uint256 i = 0; i < honoraries.length; i++) {
-            tokenIDMap[honoraryID] = honoraryID;
-            _safeMint(honoraries[i], honoraryID);
-
-            emit Claim(honoraryID);
-
+            _mint(honoraries[i], honoraryID);
             honoraryID += 1;
         }
 
         numHonoraries += honoraries.length;
     }
 
-    function setAirdropDetails(uint256 number, bytes32 root)
+    function setFreeMintDetails(uint256 _reserved, bytes32 _root)
         external
         onlyOwner
     {
-        require(minted + number <= MAX_MINTABLE);
-        airdropNumber = number;
-        merkleRoot = root;
+        require(apaGalsMinted + _reserved <= MAX_SUPPLY);
+        reserved = _reserved;
+        freeMerkleRoot = _root;
     }
 
-    function getClaimedAirdrops(address account)
+    function getClaimedFreeMints(address account)
         external
         view
         returns (uint256)
     {
-        return redeemTracker[account];
+        return freeRedeemed[account];
     }
 
-    function redeem(
-        address account,
-        uint256 totalGiven,
-        uint256 requesting,
-        bytes32[] memory proof
-    ) external nonReentrant {
-        require(
-            canClaimAirdrop,
-            "Party Animals: it's not possible to claim the airdrop at the moment."
-        );
-        require(minted + requesting <= MAX_MINTABLE);
-        require(airdropNumber >= requesting);
-        require(redeemTracker[account] + requesting <= totalGiven);
-        require(
-            _verify(_leaf(account, totalGiven), proof),
-            "Invalid merkle proof"
-        );
+    /*///////////////////////////////////////////////////////////////
+                            ERC721
+    //////////////////////////////////////////////////////////////*/
 
-        uint256 nextId = minted;
-        for (uint256 i = 0; i < requesting; i++) {
-            mintAPAGal(account, nextId);
-            emit Redeem(nextId);
-
-            nextId += 1;
-        }
-
-        minted += requesting;
-        airdropNumber -= requesting;
-        redeemTracker[account] += requesting;
-    }
-
-    function _leaf(address account, uint256 totalGiven)
-        internal
-        pure
-        returns (bytes32)
-    {
-        return keccak256(abi.encodePacked(account, totalGiven));
-    }
-
-    function _verify(bytes32 leaf, bytes32[] memory proof)
-        internal
-        view
-        returns (bool)
-    {
-        return MerkleProof.verify(proof, merkleRoot, leaf);
-    }
-
-    /* ****************** */
-    /*       ERC721       */
-    /* ****************** */
     function tokenURI(uint256 tokenId)
         public
         view
@@ -243,26 +234,67 @@ contract PartyAnimals is
             _exists(tokenId),
             "ERC721Metadata: URI query for nonexistent token"
         );
-        string memory baseURI = _baseURI();
-        uint256 imageID = tokenIDMap[tokenId];
-        return
-            bytes(baseURI).length > 0
-                ? string(abi.encodePacked(baseURI, imageID.toString()))
-                : "";
+        return string(bytes.concat(bytes(baseURI), bytes(_toString(tokenId))));
     }
 
     function setBaseUri(string memory uri) external onlyOwner {
-        baseUri = uri;
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return baseUri;
+        baseURI = uri;
     }
 
 
+
+    function _toString(uint256 value) internal pure returns (string memory) {
+        //slither-disable-next-line incorrect-equality
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            //slither-disable-next-line weak-prng
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
+    }
+
+    function enoughRandom() internal view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        // solhint-disable-next-line
+                        block.timestamp,
+                        msg.sender,
+                        blockhash(block.number)
+                    )
+                )
+            );
+    }
+
+
+
+    /*///////////////////////////////////////////////////////////////
+                            MARKETPLACE MANAGEMENT
+    //////////////////////////////////////////////////////////////*/
 
     function emergencyWithdraw() external onlyOwner {
         payable(_msgSender()).transfer(address(this).balance);
+    }
+
+
+    function toggleClaimability() external onlyOwner {
+        canClaim = !canClaim;
+    }
+
+    function toggleAirdropClaimability() external onlyOwner {
+        canClaimAirdrop = !canClaimAirdrop;
     }
 
     receive() external payable {}
@@ -286,7 +318,7 @@ contract PartyAnimals is
         string memory nftSymbol,
         string memory baseTokenURI
     ) ERC721(nftName, nftSymbol) Ownable() {
-        baseUri = baseTokenURI;
+        baseURI = baseTokenURI;
 
     }
 }
---
+++ src/Market.sol
@@ -0,0 +1,265 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
+abstract contract IERC721Full is IERC721, IERC721Enumerable, IERC721Metadata {}
+
+
+
+contract Market is Ownable {
+    IERC721Full nftContract;
+
+    uint256 constant TOTAL_NFTS_COUNT = 1000;
+
+    struct Listing {
+        bool active;
+        uint256 id;
+        uint256 tokenId;
+        uint256 price;
+        uint256 activeIndex; // index where the listing id is located on activeListings
+        uint256 userActiveIndex; // index where the listing id is located on userActiveListings
+        address owner;
+        string tokenURI;
+    }
+
+    struct Purchase {
+        Listing listing;
+        address buyer;
+    }
+
+    event AddedListing(Listing listing);
+    event UpdateListing(Listing listing);
+    event FilledListing(Purchase listing);
+    event CanceledListing(Listing listing);
+
+    Listing[] public listings;
+    uint256[] public activeListings; // list of listingIDs which are active
+    mapping(address => uint256[]) public userActiveListings; // list of listingIDs which are active
+
+    uint256 public marketFeePercent = 0;
+
+    uint256 public totalVolume = 0;
+    uint256 public totalSales = 0;
+    uint256 public highestSalePrice = 0;
+
+    bool public isMarketOpen = false;
+    bool public emergencyDelisting = false;
+
+    constructor(
+        address nft_address,
+        uint256 market_fee
+    ) {
+        require(market_fee <= 100, "Give a percentage value from 0 to 100");
+
+        nftContract = IERC721Full(nft_address);
+        marketFeePercent = market_fee;
+    }
+
+    function openMarket() external onlyOwner {
+        isMarketOpen = true;
+    }
+
+    function closeMarket() external onlyOwner {
+        isMarketOpen = false;
+    }
+
+    function allowEmergencyDelisting() external onlyOwner {
+        emergencyDelisting = true;
+    }
+
+    function totalListings() external view returns (uint256) {
+        return listings.length;
+    }
+
+    function totalActiveListings() external view returns (uint256) {
+        return activeListings.length;
+    }
+
+    function getActiveListings(uint256 from, uint256 length)
+        external
+        view
+        returns (Listing[] memory listing)
+    {
+        uint256 numActive = activeListings.length;
+        if (from + length > numActive) {
+            length = numActive - from;
+        }
+
+        Listing[] memory _listings = new Listing[](length);
+        for (uint256 i = 0; i < length; i++) {
+            Listing memory _l = listings[activeListings[from + i]];
+            _l.tokenURI = nftContract.tokenURI(_l.tokenId);
+            _listings[i] = _l;
+        }
+        return _listings;
+    }
+
+    function removeActiveListing(uint256 index) internal {
+        uint256 numActive = activeListings.length;
+
+        require(numActive > 0, "There are no active listings");
+        require(index < numActive, "Incorrect index");
+
+        activeListings[index] = activeListings[numActive - 1];
+        listings[activeListings[index]].activeIndex = index;
+        activeListings.pop();
+    }
+
+    function removeOwnerActiveListing(address owner, uint256 index) internal {
+        uint256 numActive = userActiveListings[owner].length;
+
+        require(numActive > 0, "There are no active listings for this user.");
+        require(index < numActive, "Incorrect index");
+
+        userActiveListings[owner][index] = userActiveListings[owner][
+            numActive - 1
+        ];
+        listings[userActiveListings[owner][index]].userActiveIndex = index;
+        userActiveListings[owner].pop();
+    }
+
+    function getMyActiveListingsCount() external view returns (uint256) {
+        return userActiveListings[msg.sender].length;
+    }
+
+    function getMyActiveListings(uint256 from, uint256 length)
+        external
+        view
+        returns (Listing[] memory listing)
+    {
+        uint256 numActive = userActiveListings[msg.sender].length;
+
+        if (from + length > numActive) {
+            length = numActive - from;
+        }
+
+        Listing[] memory myListings = new Listing[](length);
+
+        for (uint256 i = 0; i < length; i++) {
+            Listing memory _l = listings[
+                userActiveListings[msg.sender][i + from]
+            ];
+            _l.tokenURI = nftContract.tokenURI(_l.tokenId);
+            myListings[i] = _l;
+        }
+        return myListings;
+    }
+
+    function addListing(uint256 tokenId, uint256 price) external {
+        require(isMarketOpen, "Market is closed.");
+        require(
+            tokenId < TOTAL_NFTS_COUNT,
+            "Honorary APAs are not accepted in the marketplace"
+        );
+        require(msg.sender == nftContract.ownerOf(tokenId), "Invalid owner");
+
+        uint256 id = listings.length;
+        Listing memory listing = Listing(
+            true,
+            id,
+            tokenId,
+            price,
+            activeListings.length, // activeIndex
+            userActiveListings[msg.sender].length, // userActiveIndex
+            msg.sender,
+            ""
+        );
+
+        listings.push(listing);
+        userActiveListings[msg.sender].push(id);
+        activeListings.push(id);
+
+        emit AddedListing(listing);
+
+        nftContract.transferFrom(msg.sender, address(this), tokenId);
+    }
+
+    function updateListing(uint256 id, uint256 price) external {
+        require(id < listings.length, "Invalid Listing");
+        require(listings[id].active, "Listing no longer active");
+        require(listings[id].owner == msg.sender, "Invalid Owner");
+
+        listings[id].price = price;
+        emit UpdateListing(listings[id]);
+    }
+
+    function cancelListing(uint256 id) external {
+        require(id < listings.length, "Invalid Listing");
+        Listing memory listing = listings[id];
+        require(listing.active, "Listing no longer active");
+        require(listing.owner == msg.sender, "Invalid Owner");
+
+        removeActiveListing(listing.activeIndex);
+        removeOwnerActiveListing(msg.sender, listing.userActiveIndex);
+
+        listings[id].active = false;
+
+        emit CanceledListing(listing);
+
+        nftContract.transferFrom(address(this), listing.owner, listing.tokenId);
+    }
+
+    function fulfillListing(uint256 id) external payable {
+        require(id < listings.length, "Invalid Listing");
+        Listing memory listing = listings[id];
+        require(listing.active, "Listing no longer active");
+        require(msg.value >= listing.price, "Value Insufficient");
+        require(msg.sender != listing.owner, "Owner cannot buy own listing");
+        listings[id].active = false;
+
+        uint256 market_cut = (listing.price * marketFeePercent) / 100;
+        uint256 holder_cut = listing.price - market_cut;
+        // Update active listings
+        removeActiveListing(listing.activeIndex);
+        removeOwnerActiveListing(listing.owner, listing.userActiveIndex);
+
+        // Update global stats
+        totalVolume += listing.price;
+        totalSales += 1;
+
+        if (listing.price > highestSalePrice) {
+            highestSalePrice = listing.price;
+        }
+
+
+        emit FilledListing(
+            Purchase({listing: listings[id], buyer: msg.sender})
+        );
+        payable(listing.owner).transfer(holder_cut);
+        nftContract.transferFrom(address(this), msg.sender, listing.tokenId);
+    }
+
+
+    function adjustFees(uint256 newMarketFee)
+        external
+        onlyOwner
+    {
+        require(newMarketFee <= 100, "Give a percentage value from 0 to 100");
+        marketFeePercent = newMarketFee;
+    }
+
+    function emergencyDelist(uint256 listingID) external {
+        require(emergencyDelisting && !isMarketOpen, "Only in emergency.");
+        require(listingID < listings.length, "Invalid Listing");
+        Listing memory listing = listings[listingID];
+
+        nftContract.transferFrom(address(this), listing.owner, listing.tokenId);
+    }
+
+    function withdrawableBalance() public view returns (uint256 value) {
+        return address(this).balance;
+    }
+
+    function withdrawBalance() external onlyOwner {
+        uint256 withdrawable = withdrawableBalance();
+        payable(_msgSender()).transfer(withdrawable);
+    }
+
+    function emergencyWithdraw() external onlyOwner {
+        payable(_msgSender()).transfer(address(this).balance);
+    }
+}
